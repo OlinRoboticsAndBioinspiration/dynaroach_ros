d62f17d01df4b11030e67a3548cb35299fbc1aec
shivamdesai <shivamxdesai@gmail.com>
2014-04-29 15:48:00 -0400

    refactored into DynaRoachNode class

--- src/dynaroach_ros.py
+++ src/dynaroach_ros.py
@@ -27,75 +27,82 @@ from lib.payload import Payload
 import rospy
 from std_msgs.msg import *
 
-def receive_packet(payload, dynaroach, publisher):
-    publisher.publish(str(payload))
+import atexit
 
-def receive_time(payload, dynaroach, publisher):
-    if payload.type == cmd.DATA_STREAMING:
-        datum = list(unpack('<L3f3h2HB4H', payload.data))
-        publisher.publish(datum[0])
+class DynaRoachNode(dr.DynaRoach):
 
-def receive_gyro(payload, dynaroach, p1, p2, p3):
-    if payload.type == cmd.DATA_STREAMING:
-        datum = list(unpack('<L3f3h2HB4H', payload.data))
-        p1.publish(datum[1])
-        p2.publish(datum[2])
-        p3.publish(datum[3])
+    def __init__(self, device_name='/dev/ttyUSB0'):
 
-def receive_accel(payload, dynaroach,  p1, p2, p3):
-    if payload.type == cmd.DATA_STREAMING:
-        datum = list(unpack('<L3f3h2HB4H', payload.data))
-        p1.publish(datum[4])
-        p2.publish(datum[5])
-        p3.publish(datum[6])
+        super(DynaRoachNode, self).__init__(device_name)
 
-def receive_backemf(payload, dynaroach, publisher):
-    if payload.type == cmd.DATA_STREAMING:
-        datum = list(unpack('<L3f3h2HB4H', payload.data))
-        publisher.publish(datum[7])
+        self.pub_packet = rospy.Publisher('dynaroach/packet', String)
+        self.pub_time = rospy.Publisher('dynaroach/time', UInt32)
+        self.pub_gyro_yaw = rospy.Publisher('dynaroach/gyro/yaw', Float32)
+        self.pub_gyro_pitch = rospy.Publisher('dynaroach/gyro/pitch', Float32)
+        self.pub_gyro_roll = rospy.Publisher('dynaroach/gyro/roll', Float32)
+        self.pub_accel_x = rospy.Publisher('dynaroach/accel/x', UInt16)
+        self.pub_accel_y = rospy.Publisher('dynaroach/accel/y', UInt16)
+        self.pub_accel_z = rospy.Publisher('dynaroach/accel/z', UInt16)
+        self.pub_backemf = rospy.Publisher('dynaroach/backemf', UInt16)
+        self.pub_hall = rospy.Publisher('dynaroach/hall', Bool)
+        self.pub_batt = rospy.Publisher('dynaroach/batt', UInt16)
 
-def receive_hall(payload, dynaroach, publisher):
-    if payload.type == cmd.DATA_STREAMING:
-        datum = list(unpack('<L3f3h2HB4H', payload.data))
-        publisher.publish(datum[9])
+        rospy.init_node('dynaroach_node', anonymous=True)
 
-def receive_batt(payload, dynaroach, publisher):
-    if payload.type == cmd.DATA_STREAMING:
-        datum = list(unpack('<L3f3h2HB4H', payload.data))
-        publisher.publish(datum[10])
+        self.set_data_streaming(True)
 
-def dynaroach_node(dynaroach):
-    
-    pub_packet = rospy.Publisher('dynaroach/packet', String)
-    pub_time = rospy.Publisher('dynaroach/time', Int64)
-    pub_gyro_yaw = rospy.Publisher('dynaroach/gyro/yaw', Float64)
-    pub_gyro_pitch = rospy.Publisher('dynaroach/gyro/pitch', Float64)
-    pub_gyro_roll = rospy.Publisher('dynaroach/gyro/roll', Float64)
-    pub_accel_x = rospy.Publisher('dynaroach/accel/x', Int64)
-    pub_accel_y = rospy.Publisher('dynaroach/accel/y', Int64)
-    pub_accel_z = rospy.Publisher('dynaroach/accel/z', Int64)
-    pub_backemf = rospy.Publisher('dynaroach/backemf', Int64)
-    pub_hall = rospy.Publisher('dynaroach/hall', Bool)
-    pub_batt = rospy.Publisher('dynaroach/batt', Int64)
-    
-    rospy.init_node('dynaroach_node', anonymous=True)
-    r = rospy.Rate(.1) # 10hz
-    
-    dynaroach.set_data_streaming(True)
-    
-    dynaroach.add_receive_callback(partial(receive_packet, dynaroach=dynaroach, publisher=pub_packet))
-    dynaroach.add_receive_callback(partial(receive_time, dynaroach=dynaroach, publisher=pub_time))
-    dynaroach.add_receive_callback(partial(receive_gyro, dynaroach=dynaroach, p1=pub_gyro_yaw, p2=pub_gyro_pitch, p3=pub_gyro_roll))
-    dynaroach.add_receive_callback(partial(receive_accel, dynaroach=dynaroach, p1=pub_accel_x, p2=pub_accel_y, p3=pub_accel_z))
-    dynaroach.add_receive_callback(partial(receive_backemf, dynaroach=dynaroach, publisher=pub_backemf))
-    dynaroach.add_receive_callback(partial(receive_hall, dynaroach=dynaroach, publisher=pub_hall))
-    dynaroach.add_receive_callback(partial(receive_hall, dynaroach=dynaroach, publisher=pub_batt))
+        self.add_receive_callback(self.receive_packet)
+        self.add_receive_callback(self.receive_time)
+        self.add_receive_callback(self.receive_gyro)
+        self.add_receive_callback(self.receive_accel)
+        self.add_receive_callback(self.receive_backemf)
+        self.add_receive_callback(self.receive_hall)
+        self.add_receive_callback(self.receive_batt)
 
-    while not rospy.is_shutdown():
-        r.sleep()
+    def receive_packet(self, payload):
+        self.pub_packet.publish(str(payload))
+
+    def receive_time(self, payload):
+        if payload.type == cmd.DATA_STREAMING:
+            datum = list(unpack('<L3f3h2HB4H', payload.data))
+            self.pub_time.publish(datum[0])
+
+    def receive_gyro(self, payload):
+        if payload.type == cmd.DATA_STREAMING:
+            datum = list(unpack('<L3f3h2HB4H', payload.data))
+            self.pub_gyro_yaw.publish(datum[1])
+            self.pub_gyro_pitch.publish(datum[2])
+            self.pub_gyro_roll.publish(datum[3])
+
+    def receive_accel(self, payload):
+        if payload.type == cmd.DATA_STREAMING:
+            datum = list(unpack('<L3f3h2HB4H', payload.data))
+            self.pub_accel_x.publish(datum[4])
+            self.pub_accel_y.publish(datum[5])
+            self.pub_accel_z.publish(datum[6])
+
+    def receive_backemf(self, payload):
+        if payload.type == cmd.DATA_STREAMING:
+            datum = list(unpack('<L3f3h2HB4H', payload.data))
+            self.pub_backemf.publish(datum[7])
+
+    def receive_hall(self, payload):
+        if payload.type == cmd.DATA_STREAMING:
+            datum = list(unpack('<L3f3h2HB4H', payload.data))
+            self.pub_hall.publish(datum[9])
+
+    def receive_batt(self, payload):
+        if payload.type == cmd.DATA_STREAMING:
+            datum = list(unpack('<L3f3h2HB4H', payload.data))
+            self.pub_batt.publish(datum[10])
+
+    def run(self):
+        r = rospy.Rate(.1) # 10hz
+        while not rospy.is_shutdown():
+            r.sleep()
 
 if __name__ == '__main__':
 
-    dr = dr.DynaRoach(sys.argv[1])
+    dyn = DynaRoachNode(sys.argv[1])
 
-    dynaroach_node(dr)
+    dyn.run()
